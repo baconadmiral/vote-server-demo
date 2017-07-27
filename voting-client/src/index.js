22210c58b11e1b450e6697ad89f0d2222ab3bfc0
baconadmiral <andrewwilliams1087@gmail.com>
2017-07-27 04:45:52 -0600

    Got first gui Test Implemented
    Had to fix test_helper.js see file for details on required changes from the tutorial

--- voting-client/src/index.js
+++
@@ -1,1 +0,0 @@
-console.log('Johnny Five Is Alive!');
--- voting-client/test/test_helper.js
+++ voting-client/test/test_helper.js
@@ -1,12 +1,18 @@
-import jsdom from 'jsdom';
+//import jsdom from 'jsdom';
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
 
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
-const win = doc.defaultView;
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
-global.document = doc;
-global.window = win;
+const dom = new JSDOM(`<!DOCTYPE html><html><body></body></html>`);
+
+//Had to remove from tutorial code and replace with changes to get it to work with newer version of jsdom
+//const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+//const win = doc.defaultView;
+
+global.document = dom.window.document;
+global.window = dom.window;
 
 Object.keys(window).forEach((key) => {
   if (!(key in global)) {
